7b5ebcfa3f336c7833f58e86d224039d7c79e21e
gpariente <guypar1997@gmail.com>
2024-12-16 12:30:15 +0200

    states commit

---
+++ simulator/state.py
@@ -0,0 +1,19 @@
+from simulator.ship import Ship
+
+class State:
+    """Represents a time step in the simulation."""
+    def __init__(self, time_step, ships):
+        self.time_step = time_step
+        self.ships = ships  # List of Ship objects
+
+    def isGoalState(self):
+        """Check if all ships have reached their destinations."""
+        return all(ship.reached_destination() for ship in self.ships)
+
+    def update_ships(self, delta_seconds=1):
+        """Update the position of all ships based on a time delta (in seconds)."""
+        for ship in self.ships:
+            ship.update_position(delta_seconds)
+
+    def increment_time_step(self):
+        self.time_step += 1
